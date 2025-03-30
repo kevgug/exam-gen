144ee19a96622218794803b4f65ed61cba0520b1
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2025-03-30 05:54:39 -0500

    compute answer service

---
+++ src/server/core/services/computeAnswer.ts
@@ -0,0 +1,61 @@
+import { createOpenAI } from "@ai-sdk/openai";
+import { executeTool } from "freestyle-sandboxes/src/ai";
+
+import { Question } from "../../../shared/types/question";
+import { generateText } from "ai";
+
+const openai = createOpenAI({
+  compatibility: "strict",
+});
+const codeExecutor = executeTool({
+  apiKey: "X1WoDa3f9GBTdEKNTTwY5y-BH12wGGZQhswcakKHSzB8TLfKrZy2Ytn1zW9Fq8SRSbv",
+  nodeModules: {
+    axios: "0.21.1",
+  },
+});
+
+export class ComputeAnswerService {
+  public static async answer({
+    prevQuestions,
+    currQuestion,
+  }: {
+    prevQuestions: Question[];
+    currQuestion: Question;
+  }): Promise<string> {
+    let context = "Previous parts to this question:";
+    context +=
+      "\n" +
+      prevQuestions.map(
+        (q) =>
+          `<question>${q.content}</question><type>${q.type}</type><answer>${q.answer}</answer>`,
+      );
+
+    if (currQuestion.type === "freeform-response") {
+      // No computation necessary; generate answer traditionally
+      const { text, steps } = await generateText({
+        system:
+          "You are an expert STEM teacher. Given a question, write down just the answer/s and nothing else. Unless specified, give numerical answers to 3 s.f. with the correct units",
+        model: openai("gpt-4o"),
+        maxSteps: 5,
+        maxRetries: 0,
+        prompt: `<context>${context}</context>\nConcisely write any and all valid solutions to this question for a markscheme: <question>${currQuestion.content}</question><type>${currQuestion.type}</type>.`,
+      });
+
+      return text;
+    }
+
+    // Call Freestyle with tool
+    const { text, steps } = await generateText({
+      model: openai("gpt-4o"),
+      tools: {
+        codeExecutor,
+      },
+      maxSteps: 5,
+      maxRetries: 0,
+      prompt: `<context>${context}</context>\nUsing code executor tool, compute the answer to this STEM problem with the correct units: <question>${currQuestion.content}</question><type>${currQuestion.type}</type>.`,
+    });
+    console.log(steps);
+
+    return text;
+  }
+}
