5ca9cc141d9bfaa0d476ccf38170afb7af019014
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2025-04-04 21:45:14 -0500

    new: flat exam schema

--- src/shared/types/exam.ts
+++ src/shared/types/exam.ts
@@ -1,13 +1,43 @@
 import { Id, PointWeighting } from "./common";
 import { QuestionGroup } from "./question";
 
-export type ExamMetadata = {
-  numQuestions: number;
-  weighting: PointWeighting;
-};
+// export type ExamMetadata = {
+//   numQuestions: number;
+//   weighting: PointWeighting;
+// };
 
+// export type Exam = {
+//   generated: boolean | undefined;
+//   questionGroups: QuestionGroup[];
+//   // metadata: ExamMetadata;
+// };
+
+export type ExamPartContentSetupChunk = {
+  chunkType: "text" | "table" | "image";
+  chunkValue: string;
+};
+export type ExamPartMultipleChoiceOption = {
+  optionType: "text" | "table" | "image";
+  optionValue: string;
+};
+export type ExamPart = {
+  partName: string;
+  partLevel: number;
+  content: {
+    setupChunks: ExamPartContentSetupChunk[] | null;
+    task: string | null;
+  };
+  partType: "parent" | "question";
+  questionType: "write" | "multipleChoice" | "table" | "sketch" | null;
+  writeIsComputational: boolean | null;
+  writeNumAnswersExpected: number | null;
+  multipleChoiceOptions: ExamPartMultipleChoiceOption[] | null;
+  tableBaseMarkdown: string | null;
+  sketchBaseImage: string | null;
+  sketchBaseReferenceImage: number | null;
+  pointsAvailable: number | null;
+};
 export type Exam = {
-  generated: boolean | undefined;
-  questionGroups: QuestionGroup[];
-  // metadata: ExamMetadata;
+  generated?: boolean;
+  parts: ExamPart[];
 };
